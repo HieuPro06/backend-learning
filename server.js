24a8016a0d47502017fdace41f0b8e5818756585
HieuPro06 <phananhhieu2002@gmail.com>
2024-07-08 23:21:27 +0700

    lesson 2

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const http = require("http");
-const hostname = "localhost";
-const port = 7777;
+// const http = require("http");
+// const hostname = "localhost";
+// const port = 7777;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("Hello World");
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`);
+// server.listen(port, hostname, () => {
+//     console.log(`Server is running at http://${hostname}:${port}`);
+// })
+// #15
+const express = require("express");
+const app = express();
+const port = 7878;
+
+app.get("/", (req, res) => {
+    res.send("Server backend");
+})
+app.get("/love", (req, res) => {
+    res.send("First love , first date")
+})
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 })
